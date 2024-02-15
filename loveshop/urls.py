a0d9ab48822b46e8d6c8c16143bd19f53f3516f8
Jesse Lee Ross <jesseross001@gmail.com>
2024-02-15 16:42:01 +0000

    Add login and register

--- loveshop/urls.py
+++ loveshop/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html"), name='home'),  # Home page
-    path('accounts/', include('accounts.urls')),  # Include the accounts app URLs
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('products/', include('products.urls', namespace='products')),  # Products app
     path('contact/', include('contact.urls', namespace='contact')),  # Contact app
     path('checkout/', include('checkout.urls', namespace='checkout')),  # Checkout app

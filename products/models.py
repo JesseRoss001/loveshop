80762a646c162d7847d776a9c2a1d6bc4c0faed5
Jesse Lee Ross <jesseross001@gmail.com>
2024-02-15 19:44:36 +0000

    Add the product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
+# Define the category choices
+CATEGORY_CHOICES = (
+    ('flowers', 'Flowers'),
+    ('chocolate', 'Chocolate'),
+    ('jewellery', 'Jewellery'),
+    ('hand_crafted', 'Hand Crafted'),
+    ('original', 'Original'),
+    ('cards', 'Cards'),
+    ('decorations', 'Decorations'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    quantity_remaining = models.IntegerField(validators=[MinValueValidator(0)])
+    image = models.ImageField(upload_to='product_images/')  # Assuming you have media set up
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.name
 # Create your models here.
--- products/views.py
+++ products/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-  # Import your Product model
+from django.db.models import Q
+from .models import Product, CATEGORY_CHOICES
+from django.core.paginator import Paginator
 
 def product_list(request):
-     # Query all products
-    return render(request, 'products/product_list.html', {})
+    query = request.GET.get('query', '')
+    category = request.GET.get('category', '')
+    sort = request.GET.get('sort', '')
+    
+    products = Product.objects.all()
 
-# Create your views here.
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    if category:
+        products = products.filter(category=category)
+
+    if sort == 'price_desc':
+        products = products.order_by('-price')
+    elif sort == 'price_asc':
+        products = products.order_by('price')
+
+    paginator = Paginator(products, 9)  # Show 9 products per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'products/product_list.html', {
+        'products': page_obj,
+        'CATEGORY_CHOICES': CATEGORY_CHOICES
+    })

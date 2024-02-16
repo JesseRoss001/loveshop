92623118627f5a2f0644082488ee85da103bc3ff
Jesse Lee Ross <jesseross001@gmail.com>
2024-02-16 00:20:00 +0000

    Add cart models

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import add_to_cart, view_cart, product_list
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='index'),  # Assuming a view named product_list
+    path('', product_list, name='index'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', view_cart, name='view_cart'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.db.models import Q
 from .models import Product, CATEGORY_CHOICES
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, get_object_or_404
+from .models import Product, Cart, CartItem
 def product_list(request):
     query = request.GET.get('query', '')
     category = request.GET.get('category', '')
@@ -28,3 +30,18 @@ def product_list(request):
         'products': page_obj,
         'CATEGORY_CHOICES': CATEGORY_CHOICES
     })
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+    return redirect('product_list')
+
+@login_required
+def view_cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    return render(request, 'cart/view_cart.html', {'cart': cart})

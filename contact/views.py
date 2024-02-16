9fe56b33823ea40652dc72734928eaf972ec6046
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2024-02-16 19:43:12 +0000

    added contact_success

--- contact/views.py
+++ contact/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render, redirect
 from .forms import ContactUsForm
 
 
-def contact_success(request):
-
-    """ Render the Contact Success HTML page """
-
-    return render(request, 'contact_success')
-
-
 def contact_page(request):
     """ View function for the contact us form """
 
@@ -16,7 +9,7 @@ def contact_page(request):
         form = ContactUsForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('contact/contact_success.html')
+            return redirect('contact_success')
     else:
         form = ContactUsForm()
 
@@ -25,4 +18,11 @@ def contact_page(request):
     }
 
     return render(request, 'contact/contact_page.html', context)
+
+
+def contact_success(request):
+
+    """ Render the Contact Success HTML page """
+
+    return render(request, 'contact/contact_success.html')
 

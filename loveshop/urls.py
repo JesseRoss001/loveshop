0aa8d24689c4b60a808dc9adbb081a37381d0f6f
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2024-02-17 15:58:12 +0000

    Created about us app and finished stiling

--- loveshop/urls.py
+++ loveshop/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),  # Home page
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('products/', include('products.urls', namespace='products')),  # Products app
-
     path('checkout/', include('checkout.urls', namespace='checkout')),  # Checkout app
     path('contact/', include('contact.urls', namespace='contact')), # Contact app
+    path('about/', include('about.urls')), # About app
 ]

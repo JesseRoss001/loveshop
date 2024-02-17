235e21b58b92b80d8c266d6843dd92abfc66ecd2
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2024-02-17 23:26:55 +0000

    Accepted the new changes for about page

--- loveshop/settings.py
+++ loveshop/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 # Add the Gitpod and Heroku hostnames to ALLOWED_HOSTS  
 
 
-ALLOWED_HOSTS = ['loveshop-037a9f640521.herokuapp.com', 'localhost', '127.0.0.1', '8000-jesseross001-loveshop-l66tcfpckvd.ws-eu108.gitpod.io', '8000-enrightc-loveshop-1yvrkj5ue7l.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-ge9dv0dwqbc.ws-eu108.gitpod.io','8000-jesseross001-loveshop-x4466lrm8sk.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-vlxgoz8ar2u.ws-eu108.gitpod.io',  '8000-jesseross001-loveshop-vlxgoz8ar2u.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-nf87aulamh1.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-nf87aulamh1.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-iw1cu87l27t.ws-eu108.gitpod.io' ]
+ALLOWED_HOSTS = ['loveshop-037a9f640521.herokuapp.com', 'localhost', '127.0.0.1', '8000-jesseross001-loveshop-l66tcfpckvd.ws-eu108.gitpod.io', '8000-enrightc-loveshop-1yvrkj5ue7l.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-ge9dv0dwqbc.ws-eu108.gitpod.io','8000-jesseross001-loveshop-x4466lrm8sk.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-vlxgoz8ar2u.ws-eu108.gitpod.io',  '8000-jesseross001-loveshop-vlxgoz8ar2u.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-nf87aulamh1.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-nf87aulamh1.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-iw1cu87l27t.ws-eu108.gitpod.io', '8000-jesseross001-loveshop-84iol2m5xai.ws-eu108.gitpod.io' ]
 
 
 CSRF_TRUSTED_ORIGINS = [

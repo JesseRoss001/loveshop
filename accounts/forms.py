ceb046f4b12259c5113242be80894ed7e62efb74
monimaj89 <mn.majchrowicz@gmail.com>
2024-02-17 21:18:13 +0000

    Style login/register pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -19,3 +19,21 @@ class UserRegisterForm(UserCreationForm):
             user_profile = UserProfile.objects.create(user=user, phone_number=self.cleaned_data['phone_number'])
             user_profile.save()
         return user
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+     
+        # Sets placeholder on inputs
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Your Email'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Phone Number'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password 1'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Password 2'
+
+        self.fields['username'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            # Adds stylings classes to inputs
+            self.fields[field].widget.attrs['class'] = (
+                'mb-3 px-2 py-2 rounded shadow text-black form-border')
+            self.fields[field].label = False

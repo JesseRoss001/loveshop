64b599e430f45a74e384d34c101d587db03f81bf
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-02-18 11:45:36 +0000

    pylint contact urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.contact_page, name='index'),  # Assuming a view named contact_page
+    path(
+        '', views.contact_page, name='index'),
     path('contact_success/', views.contact_success, name='contact_success'),
-    
 ]

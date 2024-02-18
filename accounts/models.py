3d8d55b76a1f4c191f181e41d19f66eeb3868a79
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-02-18 11:27:46 +0000

    pylint accounts models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=17, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$')], blank=True)
-    postal_code = models.CharField(max_length=8, validators=[RegexValidator(regex=r'^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$', message='Please enter a valid UK postal code.')], blank=True)
+    phone_number = models.CharField(
+        max_length=17, validators=[
+            RegexValidator(regex=r'^\+?1?\d{9,15}$')], blank=True)
+    postal_code = models.CharField(max_length=8, validators=[
+        RegexValidator(regex=r'^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$',
+                       message='Please enter a valid UK postal code.')],
+                        blank=True)
     address_line_1 = models.CharField(max_length=255, blank=True)
     address_line_2 = models.CharField(max_length=255, blank=True)
     city = models.CharField(max_length=100, blank=True)
